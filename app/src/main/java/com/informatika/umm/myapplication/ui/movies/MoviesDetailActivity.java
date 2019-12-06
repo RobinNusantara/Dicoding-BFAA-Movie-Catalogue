478c9abf20dabfc35290a554c3a3fa6ff9b06202
Robin Nusantara <robinnusantara127@gmail.com>
2019-12-06 14:11:07 +0700

    Movies Detail Shimmer Layout

--- app/src/main/java/com/informatika/umm/myapplication/ui/movies/MoviesDetailActivity.java
+++ app/src/main/java/com/informatika/umm/myapplication/ui/movies/MoviesDetailActivity.java
@@ -6,13 +6,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.material.appbar.AppBarLayout;
 import com.informatika.umm.myapplication.BuildConfig;
 import com.informatika.umm.myapplication.R;
 import com.informatika.umm.myapplication.model.Movies;
@@ -20,14 +25,13 @@ import com.informatika.umm.myapplication.model.Movies;
 public class MoviesDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_MOVIE = "extra_movie";
+    ImageView imgMovieBackdrop, imgMoviePoster;
+    TextView txtMovieTitle, txtMovieRelease, txtMovieScore, txtMovieOverview;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView imgMovieBackdrop, imgMoviePoster;
-        TextView txtMovieTitle, txtMovieRelease, txtMovieScore, txtMovieOverview;
-        RatingBar ratingBar;
-        Movies movies = getIntent().getParcelableExtra(EXTRA_MOVIE);
 
         setContentView(R.layout.activity_detail_movies);
         ratingBar = findViewById(R.id.rate_movie);
@@ -38,7 +42,10 @@ public class MoviesDetailActivity extends AppCompatActivity {
         txtMovieRelease = findViewById(R.id.txt_movie_release_date);
         txtMovieScore = findViewById(R.id.txt_movie_score);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final AppBarLayout appBarLayout = findViewById(R.id.app_bar_layout);
+        appBarLayout.setVisibility(View.INVISIBLE);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.INVISIBLE);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,50 +53,79 @@ public class MoviesDetailActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("");
         }
 
-        if (movies != null) {
+        ratingBar.setVisibility(View.INVISIBLE);
 
-            String urlPoster = BuildConfig.IMAGE_URL + movies.getMoviePoster();
-            String urlBackdrop = BuildConfig.IMAGE_URL + movies.getMovieBackdrop();
+        final ShimmerFrameLayout shimmerFrameLayout = findViewById(R.id.shimmer_container);
+        shimmerFrameLayout.setVisibility(View.VISIBLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        final Handler handler = new Handler();
 
-            Glide.with(this)
-                    .load(urlPoster)
-                    .override(350, 550)
-                    .transform(new RoundedCorners(32))
-                    .into(imgMoviePoster);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Movies movies = getIntent().getParcelableExtra(EXTRA_MOVIE);
+                        if (movies != null) {
+                            appBarLayout.setVisibility(View.VISIBLE);
+                            toolbar.setVisibility(View.VISIBLE);
 
-            Glide.with(this)
-                    .load(urlBackdrop)
-                    .override(1366, 768)
-                    .into(imgMovieBackdrop);
+                            String urlPoster = BuildConfig.IMAGE_URL + movies.getMoviePoster();
+                            String urlBackdrop = BuildConfig.IMAGE_URL + movies.getMovieBackdrop();
 
-            if (movies.getMovieScore() >= 1 && movies.getMovieScore() < 2.0) {
-                ratingBar.setRating(1.0f);
-            } else if (movies.getMovieScore() >= 2.0 && movies.getMovieScore() < 5.0) {
-                ratingBar.setRating(2.0f);
-            } else if (movies.getMovieScore() >= 5.0 && movies.getMovieScore() < 6.0) {
-                ratingBar.setRating(2.5f);
-            } else if (movies.getMovieScore() >= 6.0 && movies.getMovieScore() < 7.0) {
-                ratingBar.setRating(3.0f);
-            } else if (movies.getMovieScore() >= 7.0 && movies.getMovieScore() < 8.0) {
-                ratingBar.setRating(3.5f);
-            } else if (movies.getMovieScore() >= 8.0 && movies.getMovieScore() < 9.0) {
-                ratingBar.setRating(4.0f);
-            } else if (movies.getMovieScore() >= 9.0) {
-                ratingBar.setRating(4.5f);
-            } else if (movies.getMovieScore() >= 9.5) {
-                ratingBar.setRating(5.0f);
-            } else {
-                ratingBar.setRating(0);
+                            Glide.with(MoviesDetailActivity.this)
+                                    .load(urlPoster)
+                                    .override(350, 550)
+                                    .transform(new RoundedCorners(32))
+                                    .into(imgMoviePoster);
+
+                            Glide.with(MoviesDetailActivity.this)
+                                    .load(urlBackdrop)
+                                    .override(1366, 768)
+                                    .into(imgMovieBackdrop);
+
+                            ratingBar.setVisibility(View.VISIBLE);
+                            if (movies.getMovieScore() >= 1 && movies.getMovieScore() < 2.0) {
+                                ratingBar.setRating(1.0f);
+                            } else if (movies.getMovieScore() >= 2.0 && movies.getMovieScore() < 5.0) {
+                                ratingBar.setRating(2.0f);
+                            } else if (movies.getMovieScore() >= 5.0 && movies.getMovieScore() < 6.0) {
+                                ratingBar.setRating(2.5f);
+                            } else if (movies.getMovieScore() >= 6.0 && movies.getMovieScore() < 7.0) {
+                                ratingBar.setRating(3.0f);
+                            } else if (movies.getMovieScore() >= 7.0 && movies.getMovieScore() < 8.0) {
+                                ratingBar.setRating(3.5f);
+                            } else if (movies.getMovieScore() >= 8.0 && movies.getMovieScore() < 9.0) {
+                                ratingBar.setRating(4.0f);
+                            } else if (movies.getMovieScore() >= 9.0) {
+                                ratingBar.setRating(4.5f);
+                            } else if (movies.getMovieScore() >= 9.5) {
+                                ratingBar.setRating(5.0f);
+                            } else {
+                                ratingBar.setRating(0);
+                            }
+
+                            String txtMoviesScore = Double.toString(movies.getMovieScore());
+                            txtMovieScore.setText(txtMoviesScore);
+
+                            txtMovieTitle.setText(movies.getMovieTitle());
+                            txtMovieRelease.setText(movies.getMovieRelease());
+                            txtMovieOverview.setText(movies.getMovieOverview());
+
+                            shimmerFrameLayout.setVisibility(View.INVISIBLE);
+                            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                        }
+                    }
+                });
             }
-
-            String txtMoviesScore = Double.toString(movies.getMovieScore());
-            txtMovieScore.setText(txtMoviesScore);
-
-            txtMovieTitle.setText(movies.getMovieTitle());
-            txtMovieRelease.setText(movies.getMovieRelease());
-            txtMovieOverview.setText(movies.getMovieOverview());
-
-        }
+        }).start();
     }
 
     @Override
