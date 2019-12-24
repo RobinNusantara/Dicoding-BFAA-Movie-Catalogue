fa6c6ea6ba41ab2b9d15066000ed013d64b5f281
Robin Nusantara <robinnusantara127@gmail.com>
2019-12-24 23:03:21 +0700

    add Database package

---
+++ app/src/main/java/com/informatika/umm/myapplication/database/FavoriteContract.java
@@ -0,0 +1,9 @@
+package com.informatika.umm.myapplication.database;
+
+/**
+ * MADE_Submission_2
+ * created by : Robin Nusantara on 12/24/2019 12 2019
+ * 23:02 Tue
+ **/
+public class FavoriteContract {
+}
